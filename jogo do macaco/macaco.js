1e48833c2ddeb67aedae51b7f02ba3bc16a6a170
RafaellaAmaral <127240010+RafaellaAmaral@users.noreply.github.com>
2023-11-28 11:44:14 -0300

    Update macaco.js

--- jogo do macaco/macaco.js
+++ jogo do macaco/macaco.js
@@ -1,16 +1,21 @@
+
+
 const macacoEl = document.querySelector("#macaco");
 const barrilEl= document.querySelector("#barril");
 const score = document.querySelector("#ponto");
 let alreadyJump = false;
 let count = 0;
 
+
 function jump() {
   if (!macacoEl.classList.contains("jump")) {
+    macacoEl.classList.remove("walk");
     macacoEl.classList.add("jump");
     alreadyJump = true;
 
     setTimeout(() => {
       macacoEl.classList.remove("jump");
+      macacoEl.classList.add("walk");
       alreadyJump = false;
     }, 1100);
   }
@@ -21,18 +26,26 @@ document.addEventListener("keydown", (e) => {
       jump();
     }
   });
+  
 setInterval(() => {
-  let macacoBottom = parseInt(
-    window.getComputedStyle(macacoEl).getPropertyValue("bottom")
-  );
   let barrilLeft = parseInt(
     window.getComputedStyle(barrilEl).getPropertyValue("left")
   );
 
-  if (barrilLeft > 40 && barrilLeft < 270 && macacoBottom <= 50 && !alreadyJump) {
+
+  let macacoBottom = parseInt( 
+    window.getComputedStyle(macacoEl).getPropertyValue("bottom")
+  );
+
+
+  console.log({barrilLeft, macacoBottom, alreadyJump})
+
+
+  if (barrilLeft > 160 && barrilLeft < 270 && !alreadyJump) {
     alert(`Game Over! Seu score foi: ${count}`);
     count = 0;
   }
+
 
   count++;
   score.innerHTML = `SCORE: ${count}`;
